0f237dcec7fa43914fe6240524ec3ae20506c145
niceboy2002 <54811671+niceboy2002@users.noreply.github.com>
2022-06-22 13:27:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,112 @@
 
-
 var api = "http://localhost:3000/Users";
 var user = document.getElementById("inputUser");
 var fname = document.getElementById("inputName");
 var email = document.getElementById("inputEmail");
 var date = document.getElementById("inputDate");
 var button = document.getElementById("push");
+var show = document.querySelector("#show_data");
 
 const start = () => {
-	getUser()
-  pushUser()
-}
-
+  getUser();
+  pushUser();
+};
 
 const getUser = async () => {
-  var show = document.querySelector('#show_data');
   var db = await axios.get(api);
-   var htmls =
-      db.data.map(register => {  return    `
+  var htmls = db.data.map((register) => {
+    return `
       <tr>
 	     <th scope="row" class="text-light">${register.id}</th>
-	     <td class="text-light">${register.user}</td>
+	     <td class="text-light">${register.user}djdjdj</td>
 	     <td class="text-light">${register.name}</td>
 	     <td class="text-light">${register.email}</td>
 	     <td class="text-light">${register.date}</td> 
        <td> <button type="button" class="btn btn-primary bg-danger text-light" onclick="deleteUser(${register.id})"  > Delete  </button>
-       <button type="button" class="btn btn-primary bg-warning text-light" > Change  </button></td>
+       <button type="button" class="btn btn-primary bg-warning text-light" onclick="updateUser(${register.id})" > Change  </button></td>
 	    </tr>
        `;
   });
-  show.innerHTML = htmls.join(" ")
-  console.log(db.data)
-  
+  show.innerHTML = htmls.join(" ");
 };
 
 const postUser = async (form) => {
-   try {
-        const response = await axios.post(api, form);
-   } catch(err){
-      console.log(err)
-   }
-}
+  try {
+    const response = await axios.post(api, form);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-const pushUser = async () =>{
-     button.addEventListener("click",function () {
-         var form = {
-              user:user.value,
-              name:fname.value,
-              email:email.value,
-              date:date.value
-         }
-         postUser(form);
-     })  
+const putUser = async (id,form) => {
+   try {
+     const response = await axios.put(api+ "/" + id, form)
+   } catch (err){
+     console.log(err);
+   }
 }
 
 const deleteUser = async (id) => {
   try {
-     const reomve = await axios.delete(api + "/" + id)
-  } catch(err){
-     console.log(err)
+    const reomve = await axios.delete(api + "/" + id);
+  } catch (err) {
+    console.log(err);
   }
-}
+};
 
-start()
 
+/************************************************************************************************ */
+
+const pushUser = async () => {
+  button.addEventListener("click", function () {
+    var form = {
+      user: user.value,
+      name: fname.value,
+      email: email.value,
+      date: date.value,
+    };
+    postUser(form);
+  });
+};
+
+const updateUser =  (id) => {
+  const form = `
+      <tr>
+	     <th scope="row" class="text-light"> <input required type="Id" class="form-control" id="changeId" /> </th>
+	     <td class="text-light"> <input required type="user" class="form-control" id="changeUser" /> </td>
+	     <td class="text-light"> <input required type="fullname" class="form-control" id="changeName" />  </td>
+	     <td class="text-light"> <input required type="email" class="form-control" id="changeEmail" />  </td>
+	     <td class="text-light"> <input required type="date" class="form-control" id="changeDate" />  </td> 
+       <td> 
+       <button type="button" class="btn btn-primary bg-warning text-light" id="update" > Update  
+       </td>
+	    </tr>
+       `;
+  show.innerHTML = form;
+  const btnUpdate = document.getElementById("update");
+  console.log(btnUpdate);
+  btnUpdate.addEventListener("click", function (e) {
+    e.preventDefault();
+    const Id = document.querySelector("#changeId").value;
+    const User = document.querySelector("#changeUser").value;
+    const Name = document.querySelector("#changeName").value;
+    const Email = document.querySelector("#changeEmail").value;
+    const Date = document.querySelector("#changeDate").value;
+
+    const formData = {
+      id : Id,
+      user: User,
+      name: Name,
+      email: Email,
+      date: Date,
+    };
+
+    console.log(formData)
+ 
+    putUser(id,formData)
+
+  });
+};
+
+
+start();
